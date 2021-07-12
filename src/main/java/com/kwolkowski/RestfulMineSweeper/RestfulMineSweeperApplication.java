63032ff2f099492c5e7ac382bc4597f9d3320254
kwolkowski <kacper.wolkowski@gmail.com>
2021-07-12 12:11:31 +0200

    Init project

---
+++ src/main/java/com/kwolkowski/RestfulMineSweeper/RestfulMineSweeperApplication.java
@@ -0,0 +1,13 @@
+package com.kwolkowski.RestfulMineSweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulMineSweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulMineSweeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwolkowski/RestfulMineSweeper/RestfulMineSweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwolkowski.RestfulMineSweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulMineSweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
